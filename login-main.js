f9248cf54a469f8f6a004949b6dbca52a7ee996a
Aamr-mohamed <110783762+Aamr-mohamed@users.noreply.github.com>
2023-04-25 18:55:37 +0200

    -u

---
+++ login-main.js
@@ -0,0 +1,8 @@
+var loginbutton=document.getElementsByClassName('button')
+for (var i=0;i>loginbutton.length;i++){
+    var button=loginbutton[i]
+    button.addEventListener('click',function(event){
+            var buttonClicked=event.target
+    })
+
+}
